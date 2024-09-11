fe0c0ec92d8649ba00bae7a0e76f973bca735d11
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2024-09-11 22:39:21 +0200

    fix: fixed some bugs in the API

--- src/routes/views.js
+++ src/routes/views.js
@@ -1,3 +1,17 @@
+/*
+ * DaaS-nodejs 2024 (@) Sebyone Srl
+ *
+ * File: views.js
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ * If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ * This Source Code Form is "Incompatible With Secondary Licenses", as defined by the MPL v.2.0.
+ *
+ * Contributors:
+ * vincenzo.petrungaro@gmail.com - initial implementation
+ *
+ */
+
 const express = require('express');
 const router = express.Router();
 
